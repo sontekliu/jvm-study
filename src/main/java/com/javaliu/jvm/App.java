dec8d8ee0423b4f0e85ec5a844cb805cea83a1bb
liushijun <sontek@yeah.net>
2019-03-25 13:52:32 +0800

    update java loading

--- src/main/java/com/javaliu/jvm/App.java
+++ src/main/java/com/javaliu/jvm/App.java
@@ -1,8 +1,7 @@
 package com.javaliu.jvm;
 
 public class App {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
